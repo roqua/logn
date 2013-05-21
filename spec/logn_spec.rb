9519a03cabf4ca3af6c797879d7d69fc29d97601
Marten Veldthuis <marten@veldthuis.com>
2013-05-21 21:06:37 +0200

    rename entry to event

--- spec/logn_spec.rb
+++ spec/logn_spec.rb
@@ -1,36 +1,36 @@
 # coding: utf-8
 require 'logn'
 
-describe 'parsing a log entry' do
+describe 'parsing a log event' do
   let(:line) { %(I, [2013-05-21T16:14:51.587433 #2313]  INFO -- : roqua.web:started {"uuid":"123","method":"GET","path":"/epd/patient/new_outcomes/6/scores","format":"html","controller":"epd/new_outcomes","action":"scores","status":200,"params":{"id":"6"},"session_id":"0012","session":{"logged_in_at":"2013-05-21T16:14:37+02:00","user_id":37},"duration":56.24,"view":40.91,"db":2.33}) }
-  let(:entry) { Logn::Event.new(line) }
+  let(:event) { Logn::Event.new(line) }
 
   it 'parses the level' do
-    entry.level.should == 'I'
+    event.level.should == 'I'
   end
 
   it 'parses the timestamp' do
-    entry.timestamp.should == Time.local(2013, 05, 21, 16, 14, 51, 587433)
+    event.timestamp.should == Time.local(2013, 05, 21, 16, 14, 51, 587433)
   end
 
   it 'parses the pid' do
-    entry.pid.should == '2313'
+    event.pid.should == '2313'
   end
 
   it 'parses the severity' do
-    entry.severity.should == :info
+    event.severity.should == :info
   end
 
   it 'parses the sender' do
-    entry.sender.should == 'roqua.web'
+    event.sender.should == 'roqua.web'
   end
 
   it 'parses the event' do
-    entry.event.should == 'started'
+    event.event.should == 'started'
   end
 
   it 'parses the json hash' do
-    entry.metadata.should == {
+    event.metadata.should == {
       "uuid"=> "123",
       "method"=>"GET",
       "path"=> "/epd/patient/new_outcomes/6/scores",
@@ -49,8 +49,8 @@ describe 'parsing a log entry' do
 
   it 'parses errors' do
     line = %(E, [2013-04-22T09:47:33.081972 #12790] ERROR -- : roqua.hl7.a19:failed {"exception":{},"message":"Connection timed out - connect(2)"})
-    entry = Logn::Event.new(line)
-    entry.level.should == 'E'
-    entry.severity.should == :error
+    event = Logn::Event.new(line)
+    event.level.should == 'E'
+    event.severity.should == :error
   end
 end
